8349a77493bdcd0668f38a9d9040903df0f882fc
rsuhada <suhada.robert@gmail.com>
2012-12-26 21:39:06 +0100

    added full history logging

--- custom/settings.zsh
+++ custom/settings.zsh
@@ -17,3 +17,11 @@ esac
 
 # fast cd
 eval "$(fasd --init auto)"
+
+# mega history
+function precmd() {
+    if [ "$(id -u)" -ne 0 ]; then
+        FULL_CMD_LOG="$HOME/.zshhist/zsh-history-$(date "+%Y-%m-%d").log"
+        echo "`pwd` [$(date)] `\history -1`" >> ${FULL_CMD_LOG}
+    fi
+}
